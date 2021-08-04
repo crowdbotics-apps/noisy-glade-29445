3534096895b5373704af57234b67dcbe445fe05f
Crowdbotics <team@crowdbotics.com>
2021-08-04 10:40:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "noisy_glade_29445Identifier",
+  url: "https://noisy_glade_29445Identifier.botics.co",
+  api: "https://noisy_glade_29445Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
